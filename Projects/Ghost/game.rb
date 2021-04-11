7dbf322e55cc02cd8b6b8cfe31fb8271fa9f4f3a
Charlee Wilson <charle_e@icloud.com>
2021-04-10 22:55:47 -0700

    Finish Ghost

--- Projects/Ghost/game.rb
+++ Projects/Ghost/game.rb
@@ -1,38 +1,67 @@
-require_relative "players.rb"
-require "Set"
+require_relative "players"
+# require "Set"
 
 class Game
+    attr_reader :fragment, :current_player, :players, :previous_player
+    attr_accessor :fragment
 
-    def initialize(dic, *players)
-        @players = players
+
+    def initialize(*players)
+        @players = players.map! {|player| Player.new(player)}
         @current_player = @players[0]
+        @previous_player = nil
         @fragment = ""
-        @dictionary = Set.new(dic)
+        words = File.readlines("words.txt").map(&:chomp)
+        @dictionary = Set.new(words)
     end
 
-    def add_player(player)
-        @players << player
-    end
-
-    def valid_play?(turn)
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-        alphabet.include?(turn) && dictionary_valid?(turn)
+    def players
+        @players.map {|player| player.name}
     end
 
     def dictionary_valid?(turn)
-        @dictionary.keys.any? { |key| key.starts_with?(turn)}
+        @dictionary.any? { |key| key.start_with?(turn)}
     end
 
     def lose?
-        @dictionary.include?(fragment) || !dictionary_valid(fragment)
+        @dictionary.include?(@fragment) || !dictionary_valid?(@fragment)
     end
+
+    def switch_player
+        @players.rotate!
+        @current_player = @players[0]
+    end
+
 
     def run
-        until self.lose?
-        guess = @current_player.get_guess
-        @fragment += guess
-        self.switch_player
+        until @players.length == 1
+            self.round
         end
+
+        puts @current_player.name + " You Win!!!!"
     end
 
+    def round
+        puts "New Round "
+        until self.lose?
+            puts "Fragment = " + @fragment.to_s
+            puts "Enter a letter guess , " + self.current_player.name.to_s.capitalize + ":"
+            guess = self.current_player.get_guess
+            @fragment += guess
+            puts "=============================="
+            @previous_player = @current_player
+            self.switch_player
+        end
+        self.set_up_next_round
+    end
+
+    def set_up_next_round
+        @fragment = ""  
+        @previous_player.losses += 1
+        puts @previous_player.name.to_s + "'s remaining chances - " + "GHOST"[0...@previous_player.losses]
+        if @previous_player.losses == 5
+            puts @previous_player.name.to_s + " You're out!"
+            @players.delete(@previous_player)
+        end
+    end
 end
--- Projects/Ghost/players.rb
+++ Projects/Ghost/players.rb
@@ -1,8 +1,27 @@
 
 class Player
+    attr_reader :name
+    attr_accessor :losses
 
     def initialize(name)
         @name = name
+        @losses = 0
     end
+
+    def get_guess
+        guess = nil
+        until valid_guess?(guess)
+            puts "Please enter a valid letter"
+            guess = gets.chomp
+        end
+        guess
+    end
+
+    def valid_guess?(guess)
+        return false if guess.nil?
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        alphabet.include?(guess)
+    end
+
 
 end
